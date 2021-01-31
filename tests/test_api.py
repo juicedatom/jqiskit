2d6c1fbaddc0ab9e7bc92e9983396f2247411d01
Joshua Manela <juicedatom@gmail.com>
2021-01-31 09:27:25 -0800

    Fix some tolerance errors from python 3.8.5

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,7 +15,7 @@ def test_simple_program() -> None:
 
     assert len(counts) == 1
     assert counts['0'] == 100
-    np.testing.assert_allclose(state, [1.0, 0.0])
+    np.testing.assert_allclose(state, [1.0, 0.0], atol=1e-8)
 
     counts, state = qc.measure(num_shots=100,
                                feed_dict={},
@@ -24,24 +24,24 @@ def test_simple_program() -> None:
     # Should still be a no-op on the '1' state.
     assert len(counts) == 1
     assert counts['1'] == 100
-    np.testing.assert_allclose(state, [0.0, 1.0])
+    np.testing.assert_allclose(state, [0.0, 1.0], atol=1e-8)
 
     # Adding an h gate should bring the circuit into superposition.
     qc.h(0)
     counts, state = qc.measure(num_shots=100)
     assert len(counts) == 2
-    np.testing.assert_allclose(state, [1 / np.sqrt(2), 1 / np.sqrt(2)])
+    np.testing.assert_allclose(state, [1 / np.sqrt(2), 1 / np.sqrt(2)], atol=1e-8)
 
     # Adding another should bring us out of super position.
     qc.h(0)
     counts, state = qc.measure(num_shots=100)
     assert len(counts) == 1
     assert counts['0'] == 100
-    np.testing.assert_allclose(state, [1.0, 0.0])
+    np.testing.assert_allclose(state, [1.0, 0.0], atol=1e-8)
 
     # This is the same example from the example.
     qc = QuantumCircuit(2)
     qc.h(0)
     qc.cx(0, 1)
     _, state = qc.measure(num_shots=100)
-    np.testing.assert_allclose(state, [1 / np.sqrt(2), 0., 0., 1 / np.sqrt(2)])
+    np.testing.assert_allclose(state, [1 / np.sqrt(2), 0., 0., 1 / np.sqrt(2)], atol=1e-8)
