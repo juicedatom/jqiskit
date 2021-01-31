007c54a8441856d8b6d29d9e491c768e5b26cf06
Joshua Manela <juicedatom@gmail.com>
2021-01-31 09:10:25 -0800

    Fix the setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example framework for learning quantum computing.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/juicedatom/jqiskit",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -22,5 +22,5 @@ setuptools.setup(
         'sympy',
         'numpy',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
